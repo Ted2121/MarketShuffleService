1e57e59df884681f70eed6f5c30a8852e9bb2592
Ted2121 <andrei.teodor1372@gmail.com>
2024-02-04 15:30:32 +0100

    recipe

---
+++ MarketShuffleServiceSolution/MarketShuffleService/Data Access/IRecipeItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using MarketShuffleModels;
+
+namespace MarketShuffleService.Data_Access;
+
+public interface IRecipeItemRepository
+{
+    Task<IEnumerable<RecipeItem>> GetAllRecipeItemsAsync();
+    Task<Item> GetRecipeItemByIdAsync(string id);
+    Task<string> CreateRecipeItemAsync(RecipeItem item);
+    Task<bool> UpdateRecipeItemAsync(RecipeItem item);
+    Task<bool> DeleteRecipeItemAsync(string id);
+}
