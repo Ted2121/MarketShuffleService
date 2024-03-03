3dd14d3e0254ceca9c4dfc39e322a8ef432eebff
Ted2121 <andrei.teodor1372@gmail.com>
2024-03-03 19:47:52 +0100

    context

--- MarketShuffleServiceSolution/MarketShuffleService/Data Access/AppDbContext.cs
+++ MarketShuffleServiceSolution/MarketShuffleService/Data Access/AppDbContext.cs
@@ -53,5 +53,32 @@ public class AppDbContext : DbContext
             .HasForeignKey(e => e.ParentItemId)
             .OnDelete(DeleteBehavior.Cascade);
         });
+
+        modelBuilder.Entity<Guild>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired();
+            entity.Property(e => e.Level).IsRequired();
+            entity.Property(e => e.Description).IsRequired();
+            entity.Property(e => e.Difficult).IsRequired();
+        });
+
+        modelBuilder.Entity<Player>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired();
+            entity.Property(e => e.Class).IsRequired();
+            entity.Property(e => e.Level).IsRequired();
+            entity.Property(e => e.Exoed).IsRequired();
+            entity.Property(e => e.Description).IsRequired();
+            entity.Property(e => e.SeenOnline).IsRequired();
+            entity.Property(e => e.Ap).IsRequired();
+            entity.Property(e => e.Mp).IsRequired();
+
+            entity.HasOne(e => e.Guild)
+            .WithMany(e => e.Players)
+            .HasForeignKey(e => e.GuildId)
+            .OnDelete(DeleteBehavior.Cascade);
+        });
     }
 }
