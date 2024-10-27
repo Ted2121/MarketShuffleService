2356793c116e27c5c9522fa6a79db51398613ba3
Ted2121 <andrei.teodor1372@gmail.com>
2024-10-27 12:34:34 +0100

    again

--- MarketShuffleServiceSolution/MarketShuffleService/Controllers/RecipeListRowsController.cs
+++ MarketShuffleServiceSolution/MarketShuffleService/Controllers/RecipeListRowsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MarketShuffleService.Controllers;
 
+[ApiController]
 public class RecipeListRowsController : ControllerBase
 {
     private readonly IRecipeListRowRepository _recipeListRowRepository;
