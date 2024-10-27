00e60b639a9c42a5d521b45371c3ac64d887b855
Ted2121 <andrei.teodor1372@gmail.com>
2024-10-27 12:45:59 +0100

    fixed get all recipe lists

--- MarketShuffleServiceSolution/MarketShuffleService/DTOs/RecipeListDto.cs
+++ MarketShuffleServiceSolution/MarketShuffleService/DTOs/RecipeListDto.cs
@@ -1,8 +1,11 @@
-﻿namespace MarketShuffleService.DTOs;
+﻿using MarketShuffleModels;
+
+namespace MarketShuffleService.DTOs;
 
 public class RecipeListDto
 {
     public string? Id { get; set; }
     public string? Name { get; set; }
     public string? Note { get; set; }
+    public IEnumerable<RecipeListRowDto>? Rows { get; set; }
 }
